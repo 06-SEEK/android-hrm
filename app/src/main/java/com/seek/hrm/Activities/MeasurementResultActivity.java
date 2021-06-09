a42fee28086e380435ab0bb5af5a5b558db724c2
dinhhanpw <dinhhan26031999@gmail.com>
2021-06-09 18:06:20 +0700

    measurement result UI

---
+++ app/src/main/java/com/seek/hrm/Activities/MeasurementResultActivity.java
@@ -0,0 +1,16 @@
+package com.seek.hrm.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.seek.hrm.R;
+
+public class MeasurementResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measurement_result);
+
+    }
+}
